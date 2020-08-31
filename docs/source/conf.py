362b5f8f392d9642404730fa3c24788c7efc4fcb
Jérôme BUISINE <contact@jeromebuisine.fr>
2020-08-31 14:59:36 +0200

    update available modules into package setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,9 +23,9 @@ copyright = '2020, Jérôme BUISINE'
 author = 'Jérôme BUISINE'
 
 # The short X.Y version
-version = '0.2.3'
+version = '0.3.0'
 # The full version, including alpha/beta/rc tags
-release = 'v0.2.3'
+release = 'v0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
