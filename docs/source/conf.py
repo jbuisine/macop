b25253483049bdc307cc99a0e53e4018628fbd40
Jérôme BUISINE <contact@jeromebuisine.fr>
2020-12-11 12:08:33 +0100

    fix pypi workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,9 +23,9 @@ copyright = '2020, Jérôme BUISINE'
 author = 'Jérôme BUISINE'
 
 # The short X.Y version
-version = '1.0.2'
+version = '1.0.3'
 # The full version, including alpha/beta/rc tags
-release = 'v1.0.2'
+release = 'v1.0.3'
 
 
 # -- General configuration ---------------------------------------------------
